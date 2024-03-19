902e3e26c54633630e5c34320ad239b060c5836a
Rory Jarrard <rjarrard@frontsteps.com>
2024-03-19 13:18:07 -0600

    add login form

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,9 +1,38 @@
 <template>
-  <p>login</p>
+  <form @submit.prevent="handleSubmit">
+    <h3>Login</h3>
+    <input type="email" placeholder="Email" v-model="email" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <div v-if="error" class="error">{{ error }}</div>
+    <button>Log in</button>
+  </form>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import useLogin from "@/composables/useLogin";
+export default {
+  setup() {
+    const { error, login } = useLogin();
+
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = async () => {
+      const res = await login(email.value, password.value);
+      if (!error.value) {
+        console.log("user logged in", res);
+      }
+    };
+
+    return {
+      email,
+      password,
+      error,
+      handleSubmit,
+    };
+  },
+};
 </script>
 
 <style>
