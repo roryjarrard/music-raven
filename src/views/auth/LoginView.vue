75929a7a8e78838c5caf0af03970e3e9eb003dd2
Rory Jarrard <rjarrard@frontsteps.com>
2024-04-09 10:50:45 -0600

    add isPending ref to composables

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -4,7 +4,8 @@
     <input type="email" placeholder="Email" v-model="email" />
     <input type="password" placeholder="Password" v-model="password" />
     <div v-if="error" class="error">{{ error }}</div>
-    <button>Log in</button>
+    <button v-if="!isPending">Log in</button>
+    <button v-if="isPending" disabled>Loading...</button>
   </form>
 </template>
 
@@ -13,12 +14,16 @@ import { ref } from "vue";
 import useLogin from "@/composables/useLogin";
 export default {
   setup() {
-    const { error, login } = useLogin();
+    const { error, login, isPending } = useLogin();
 
     const email = ref("");
     const password = ref("");
 
     const handleSubmit = async () => {
+      if (!email.value || !password.value) {
+        error.value = "Please fill in both fields";
+        return;
+      }
       const res = await login(email.value, password.value);
       if (!error.value) {
         console.log("user logged in", res);
@@ -28,6 +33,7 @@ export default {
     return {
       email,
       password,
+      isPending,
       error,
       handleSubmit,
     };
